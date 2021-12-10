89867044357304718cd97f5277a42daf665f60da
Jimmy Oliva <olivajames110@gmail.com>
2021-12-10 13:24:13 -0500

    Page Layout WIP

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,26 +1,45 @@
-import { Grid, Box } from "@mui/material";
+import { Grid, Box, createTheme, ThemeProvider } from "@mui/material";
 import React from "react";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Sidebar } from "./Sidebar";
 
+const themeOptions = {
+  palette: {
+    primary: {
+      main: "#601dea",
+    },
+  },
+  typography: {
+    fontSize: 12,
+  },
+};
+
+const theme = createTheme(themeOptions);
+
 const index: React.FC = (props) => {
   return (
-    <Box display="flex">
-      <Box flexShrink={0} minHeight="100vh">
-        <Sidebar />
-      </Box>
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Box sx={{ background: "#F6F4F4" }} display="flex">
+        {/* <Box flexShrink={0} minHeight="100vh">
+          <Sidebar />
+        </Box> */}
 
-      <Box display="flex" flexDirection="column" width="100%" minHeight="100vh">
-        <Header />
+        <Box
+          display="flex"
+          flexDirection="column"
+          width="100%"
+          minHeight="100vh"
+        >
+          <Box flex={1}>
+            <main>{props.children}</main>
+          </Box>
 
-        <Box flex={1}>
-          <main>{props.children}</main>
+          <Footer />
         </Box>
-
-        <Footer />
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 };
 
--- src/pages/Domain.tsx
+++ src/pages/Domain.tsx
@@ -1,20 +1,69 @@
+import { Search } from "@mui/icons-material";
+import { css } from "@emotion/css";
 import {
   Alert,
+  Button,
   Card,
   CardContent,
   Grid,
+  InputLabel,
   Snackbar,
   TextField,
 } from "@mui/material";
 import { Box } from "@mui/system";
+
 import { NextPage } from "next";
 import React, { useState } from "react";
 
+const buttonStyle = css`
+    width: 130px;
+    height: 50px;
+    border: none;
+    padding: 12px 16px;
+    border-radius: 0;
+    border-top-right-radius: 2px;
+    border-bottom-right-radius: 2px;
+    background-color: #312F2F;
+    color: #ffffff;
+    font-size: 14px;
+    /* transition: 140ms; */
+    display: flex; 
+    justify-content: space-evenly;
+    align-items: center;
+    cursor: pointer;
+    svg {
+      display: block;
+    }
+    &:hover: {
+      background: #9d9b9b ;
+    },
+`;
+
+const DomainCard = (props) => {
+  return (
+    <>
+      <span>
+        <Search />
+      </span>
+      <span>{props.title}</span>
+      <Card>
+        <CardContent>{props.children}</CardContent>
+      </Card>
+    </>
+  );
+};
 export const Domain: NextPage = () => {
   const [isError, setIsError] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
+  const [domainName, setDomainName] = useState("");
+
+  const domainSearchHandler = (e) => {
+    e.preventDefault();
+    setDomainName(e.target.name);
+  };
+
   return (
-    <Box padding={2}>
+    <Box>
       <Snackbar
         open={isError}
         onClose={() => setIsError(false)}
@@ -25,20 +74,94 @@ export const Domain: NextPage = () => {
         </Alert>
       </Snackbar>
 
-      <TextField label="Domain Name" />
+      {/* Search container  */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          padding: "30px",
+          background: "#ffffff",
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: "840px",
+            margin: "0 auto",
+            padding: "22px 0",
+          }}
+        >
+          <InputLabel
+            sx={{ fontWeight: "bold", marginBottom: "3px" }}
+            htmlFor={"domain-search"}
+          >
+            Enter Your Domain
+          </InputLabel>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
 
-      <Grid container>
-        <Grid item xs={6}>
-          <Card>
-            <CardContent>wwww</CardContent>
-          </Card>
+              background: "#ffffff",
+            }}
+          >
+            <input
+              style={{
+                width: "100%",
+                height: "50px",
+                border: "1px solid #AFAFAF",
+                padding: "12px 16px",
+                // borderRadius: "2px",
+              }}
+              value={domainName}
+              onChange={domainSearchHandler}
+              type="text"
+              name="domain-search"
+              id=""
+            />
+            <button className={buttonStyle}>
+              <span>
+                <Search fontSize="inherit" />
+              </span>
+              <span style={{ fontWeight: "bold" }}>Search</span>
+            </button>
+          </Box>
+        </Box>
+      </Box>
+      {/* End Search container  */}
+
+      <Box padding={2}>
+        <Grid spacing={2} container>
+          {/* Main Body Col   */}
+          <Grid item xs={10}>
+            <Grid
+              sx={{ maxWidth: "1080px", margin: "0 auto" }}
+              spacing={3}
+              container
+            >
+              <Grid item xs={4}>
+                <DomainCard title="Summary">Content</DomainCard>
+              </Grid>
+              <Grid item xs={4}>
+                {" "}
+                <DomainCard title="WHOIS">Content</DomainCard>
+              </Grid>
+              <Grid item xs={4}>
+                {" "}
+                <DomainCard title="DNS">Content</DomainCard>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          {/* Recent Search Col  */}
+          <Grid item xs={2}>
+            <h3>Recent Searches</h3>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardContent>wwww</CardContent>
-          </Card>
-        </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 };
